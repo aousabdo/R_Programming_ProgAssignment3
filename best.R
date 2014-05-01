9fba752ad6f37c18e9d733486199ad1bf2877e5a
aousabdo <aousabdo@gmail.com>
2014-04-30 23:58:15 -0400

    first commit

---
+++ best.R
@@ -0,0 +1,23 @@
+best <- function(state, outcome) {
+  ## List of outcomes
+  outcomelist <- c("heart attack", "heart failure", "pneumonia")
+  ## Read outcome data
+  outcomedata <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  ## Check that state and outcome are valid
+  if(!(state %in% outcomedata$State)){
+    stop("invalid state")
+  }
+  if(!(outcome %in% outcomelist)){
+    stop("invalid outcome")
+  }
+  
+  Min          <- min(outcomedata[,outcome], na.rm=TRUE)
+  HospitalName <- outcomedata[Min,]$Hospital.Name
+  cat(HospitalName)
+  
+  
+  ## Return hospital name in that state with lowest 30-day death
+  ## rate
+}
+
+
